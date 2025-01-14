d60328d5f3daca087a6058089b2e32d107b04c04
Therese H <therese.fredriksson90@gmail.com>
2025-01-14 12:43:41 +0100

    Added partials header and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.ejs"],
+  content: ["./views/**/*.ejs"], // Ser till att Tailwind scannar dina .ejs-filer
   theme: {
-    extend: {fontFamily: {
-      jaro: ['Jaro', 'sans-serif'],
-      roboto: ['Roboto', 'sans-serif'],},
+    extend: {
+      fontFamily: {
+        jaro: ["Jaro", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
-},
   plugins: [],
-}
-
+};
