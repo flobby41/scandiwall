67ecd73af7402af26a67d4f31976ec5700b57810
flobby41 <florian.cheheb@gmail.com>
2025-01-15 15:54:39 +0100

    slugs done

--- routes/products.js
+++ routes/products.js
@@ -5,7 +5,7 @@ const db = require('../db/db');
 
 /* GET product details page. */
 router.get('/product-details', function(req, res, next) {
-  res.render('product-details', { title: 'Product Details' });
+  res.render('product-details');
 });
 
 router.get('/productsAPI', (req, res)=>{
@@ -19,6 +19,21 @@ router.get('/productsAPI', (req, res)=>{
     }
   });
 });
+router.get('/products/:slug', (req, res) => {
+  const slug = req.params.slug.toLowerCase();
 
+  const mainProductSql = 'SELECT id, name, price, image, slugs FROM products WHERE slugs = ?';
+
+  db.get(mainProductSql, [slug], (err, product) => {
+    if (err) {
+      console.error("Error fetching product:", err);
+      res.status(500).send("Internal Server Error");
+    } else if (product) {
+      res.render('product-details', { product });
+    } else {
+      res.status(404).send('Product not found');
+    }
+  });
+});
 
 module.exports = router;
