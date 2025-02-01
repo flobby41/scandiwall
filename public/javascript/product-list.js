140de2750f5fea40094b9201f1c6f5bdce4802cb
flobby41 <florian.cheheb@gmail.com>
2025-02-01 06:28:27 +0100

    fix: repaired cart functionality on /product-list and /category pages

--- public/javascript/product-list.js
+++ public/javascript/product-list.js
@@ -76,83 +76,14 @@ document.addEventListener("DOMContentLoaded", () => {
     updateCartBadge();
   };
 
-  const generateProductHTML = (products, category) => `
-  <section class="product-section">
-    <h2>${category}</h2>
-    <div class="product-grid">
-      ${shuffle(products)
-        .filter((product) => product.category === category)
-        .slice(0, 4)
-        .map(
-          (product) => `
-          <div class="product-card" data-id="${product.id}">
-            <div class="product-grid-container">
-              <a href="/products/${product.slugs}">
-                <img
-                  src="${product.image}"
-                  alt="${product.name} produktbild"
-                  class="rounded-lg"
-                />
-              </a>
-              <div class="icon-container icon-button">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="add-to-cart-btn size-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                  />
-                </svg>
-              </div>
-            </div>
-            <div class="product-information">
-              <h2 class="nameF">${product.name}</h2>
-              <p class="priceF">${product.price} SEK</p>
-            </div>
-          </div>
-            
-          `
-        )
-        .join("")}
-      ${
-        products.filter((product) => product.category === category).length > 4
-          ? `
-        `
-          : ""
-      }
-    </div>
-    <div class="flex justify-center sm:justify-start">
-      <a href="/category/${category}" class=" sm:min-w-full">
-        <button class="primary-button w-full">
-          Se fler
-        </button>
-      </a>
-    </div>
-  </section>
-`;
+ 
 
-  function shuffle(array) {
-    for (let i = array.length - 1; i >= 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
-
+  
   fetch("/api/products")
     .then((resp) => resp.json())
     .then((data) => {
-      const productCard = document.getElementById("data-output");
-      productCard.innerHTML =
-        generateProductHTML(data, "Nyheter") +
-        generateProductHTML(data, "Vinter") +
-        generateProductHTML(data, "Landskap");
+      const productCard = document.getElementById("wesh");
+
 
       productCard.addEventListener("click", (e) => {
         if (e.target && e.target.classList.contains("add-to-cart-btn")) {
